147b90ec998bbba32e952ec23df246b9fe589fb7
torquecoder <torquecode@gmail.com>
2017-06-16 11:28:21 +0530

    code updated in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,12 @@ export class Hero {
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`
+    template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`
 })
 export class AppComponent {
     title = 'Tour of Heroes';
-    hero = 'Windstorm';
+    hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
 }
