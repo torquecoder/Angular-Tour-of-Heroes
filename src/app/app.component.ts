44272db005c3785018e7a9dbe27db3975ff809ce
torquecoder <torquecode@gmail.com>
2017-06-16 12:02:09 +0530

    ngModel added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,15 @@ export class Hero {
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>`
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div>
+            <label>name: </label>
+            <input [(ngModel)]="hero.name" placeholder="name" />
+        </div>
+        `
 })
 export class AppComponent {
     title = 'Tour of Heroes';
