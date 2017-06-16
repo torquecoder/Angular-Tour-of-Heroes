8e1acb0f0c6f636e978929276d62dd86fb86952e
torquecoder <torquecode@gmail.com>
2017-06-16 11:24:50 +0530

    Hero class created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+    id: number;
+    name: string;
+}
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    template: `<h1>{{title}}</h1><h2>{{hero}} details!</h2>`
 })
 export class AppComponent {
-  title = 'app';
+    title = 'Tour of Heroes';
+    hero = 'Windstorm';
 }
